41cdfb7e584968e3ef214ed14d55e729bc0ee9f0
larryjcool2001 <larryjcool2001@yahoo.com>
2026-04-02 17:47:17 +0100

    fifth commit

---
+++ var.java
@@ -0,0 +1,20 @@
+public class Var {
+    public static void main(String[] args) {
+        int hello_world  = 5;
+        int a = 10;
+        String b = "Hello";
+        double c = 3.14;
+        boolean d = true;
+        char e = 'A';
+
+        int sum = hello_world + a;
+
+        System.out.println("Hello World: " + hello_world);
+        System.out.println("Integer value: " + a);
+        System.out.println("String value: " + b);
+        System.out.println("Double value: " + c);
+        System.out.println("Sum: " + sum);
+        System.out.println("Boolean value: " + d);
+        System.out.println("Character value: " + e);
+    }
+}
